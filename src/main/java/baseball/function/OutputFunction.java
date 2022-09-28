1beebdf58eac0e7617e998b47e85245a98ed81c5
Dominic <ydj414Wnaver.com>
2022-09-28 23:14:12 +0900

    인/아웃 구현

---
+++ src/main/java/baseball/function/OutputFunction.java
@@ -0,0 +1,11 @@
+package baseball.function;
+
+public class OutputFunction {
+    public static void print(Object msg){
+        System.out.print(msg);
+    }
+
+    public static void println(Object msg){
+        System.out.println(msg);
+    }
+}
