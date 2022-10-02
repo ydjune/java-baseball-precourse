b5a57fae5e073d39a812946b6614704aa0e15a93
Dominic <ydj414Wnaver.com>
2022-10-02 23:03:44 +0900

    중요 변경 사항 : 컴퓨터 객체 생성 시 랜덤값이 중복값이 안나오도록 수정
      AS-IS : - 간헐적으로 컴퓨터객체 생성시 랜덤값의 중복이 생겨남
      TO-BE :
      - 순서를 보장하며 중복을 허용하지 않는 링크드 해시셋을 이용하여 중복 제거

--- src/main/java/baseball/domain/computer/Computer.java
+++ src/main/java/baseball/domain/computer/Computer.java
@@ -1,15 +1,55 @@
 package baseball.domain.computer;
 
+import baseball.io.OutputFunction;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.List;
+import java.util.*;
 
 public class Computer {
 
     public static List<Integer> RANDOM_NUMBER;
 
+    private final int TOTAL_SIZE = 3;
+    private final int START_VALUE = 1;
+    private final int END_VALUE = 9;
+
     public Computer(){
-        RANDOM_NUMBER = Randoms.pickUniqueNumbersInRange(100, 999, 1);
+        RANDOM_NUMBER = generatePickRndNumber();
+    }
+
+
+    public List<Integer> generatePickRndNumber(){
+        validateRange(START_VALUE, END_VALUE);
+        int rndNum=0;
+
+        Set<Integer> rightRndNum = new LinkedHashSet<>();
+        List<Integer> pickedRndNumber = new ArrayList<>();
+        while (rightRndNum.size() < TOTAL_SIZE) {
+            rndNum  = Randoms.pickNumberInRange(1, 9);
+            rightRndNum.add(rndNum);
+        }
+
+        Iterator<Integer> iterator = rightRndNum.iterator();
+        String strRndNum="";
+        while (iterator.hasNext()) {
+            strRndNum += String.valueOf(iterator.next());
+        }
+
+        pickedRndNumber.add(Integer.parseInt(strRndNum));
+        return pickedRndNumber;
+    }
+
+
+    private void validateRange(final int startInclusive, final int endInclusive) {
+        if (startInclusive > endInclusive) {
+            throw new IllegalArgumentException("startInclusive cannot be greater than endInclusive.");
+        }
+        if (endInclusive == Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("endInclusive cannot be greater than Integer.MAX_VALUE.");
+        }
+        if (endInclusive - startInclusive >= Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("the input range is too large.");
+        }
     }
 
 
---
+++ src/test/java/baseball/domain/computer/ComputerTest.java
@@ -0,0 +1,24 @@
+package baseball.domain.computer;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComputerTest {
+
+
+    @Test
+    public void 랜덤값_사이즈_테스트(){
+        Computer com = new Computer();
+
+        List<Integer> actualList = com.RANDOM_NUMBER;
+
+        String strActual = String.valueOf(actualList.get(0));
+
+        assertThat(strActual).hasSize(3);
+    }
+
+}
