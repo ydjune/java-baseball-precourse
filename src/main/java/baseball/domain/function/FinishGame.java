3718e828f20e1bf413da89fc5af7e283276e44b9
Dominic <ydj414Wnaver.com>
2022-10-02 20:41:31 +0900

    중요 변경 사항 : 게임 종료시 종료가 될 수 있도록 펑션 추가
      AS-IS : - 종료 기능 없음
      TO-BE :
      - 게임이 완료시에 종료가 될 수 있도록 엔드 포인드 생성

--- src/main/java/baseball/domain/function/FinishGame.java
+++ src/main/java/baseball/domain/function/FinishGame.java
@@ -1,19 +1,29 @@
 package baseball.domain.function;
 
 import baseball.game.Context;
+import baseball.io.InputFunction;
+import baseball.io.OutputFunction;
 
 public class FinishGame implements Functions {
 
 
     @Override
     public Functions command(Context context) {
-        return null;
+        //게임 재진행 여부에 따라 종료 또는 재스타트 여부 확인
+
+        OutputFunction.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        String command = InputFunction.readLine();
+        if(command.equals("2")){
+            return new ExitGame();
+        }
+
+        return new GameStart();
     }
 
 
     @Override
     public boolean isPlay() {
-        return false;
+        return true;
     }
 
 
